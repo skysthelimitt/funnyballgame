d40f506781c6ad7b2680f551c507d8c8b46b1936
skysthelimitt <skysth3limit@pm.me>
2023-12-19 15:00:40 -0500

    teleport portal, timer aligned with frames, laps

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ let clickX = 0;
 let clickY = 0;
 let playerX = 0;
 let playerY = 0;
-let state = 1;
-let arrowRight = new Image(); // Create new img element
+let state = 0;
+let hitFlags = [];
+let arrowRight = new Image();
 arrowRight.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'%3E%3Cpath fill='%23ffffff' d='m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z'/%3E%3C/svg%3E";
-let arrowLeft = new Image(); // Create new img element
+let arrowLeft = new Image();
 arrowLeft.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24' transform='rotate(180)'%3E%3Cpath fill='%23ffffff' d='m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z'/%3E%3C/svg%3E";
-let arrowUp = new Image(); // Create new img element
+let arrowUp = new Image();
 arrowUp.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24' transform='rotate(270)'%3E%3Cpath fill='%23ffffff' d='m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z'/%3E%3C/svg%3E";
-let arrowDown = new Image(); // Create new img element
+let arrowDown = new Image();
 arrowDown.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24' transform='rotate(90)'%3E%3Cpath fill='%23ffffff' d='m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z'/%3E%3C/svg%3E";
 let restartBtn = new Image();
 restartBtn.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'%3E%3Cpath fill='%23ffffff' d='M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z'/%3E%3C/svg%3E";
@@ -46,7 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
     mouseY = e.clientY;
   });
   function draw() {
-    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.fillStyle = "#111111";
@@ -88,17 +88,16 @@ function drawPlayer(ctx) {
 }
 function timer(ctx) {
   if (!finish) {
-    currentTime = new Date().getTime();
     if (restart) {
-      firstTime = new Date().getTime();
+      time = 0;
     }
-    time = (currentTime - firstTime) / 1000;
+    time = Math.round(time + 1000 / 30);
   }
   ctx.fillStyle = "#00000088";
   ctx.font = "bold 30px Arial";
-  ctx.fillRect(5, 5, ctx.measureText(time).width + 10, 30);
+  ctx.fillRect(5, 5, ctx.measureText(time / 1000).width + 10, 30);
   ctx.fillStyle = "white";
-  ctx.fillText(time, 10, 30);
+  ctx.fillText(time / 1000, 10, 30);
 }
 async function drawLevel(ctx) {
   if (!curLevel || !(curLevel["levelName"] == level)) {
@@ -125,6 +124,47 @@ async function drawLevel(ctx) {
     ctx.fillRect(flag1, flag2, flag3, flag4);
     if (RectCircleColliding({ x: playerX, y: playerY, r: playerSize }, { x: flag1, y: flag2, w: flag3, h: flag4 })) {
       finish = true;
+    }
+  }
+  if (curLevel["levelData"]["teleportPortal"]) {
+    for (let i = 0; i < curLevel["levelData"]["teleportPortal"].length; i++) {
+      ctx.fillStyle = "green";
+      v1 = curLevel["levelData"]["teleportPortal"][i][0][0];
+      v2 = curLevel["levelData"]["teleportPortal"][i][0][1];
+      v3 = curLevel["levelData"]["teleportPortal"][i][0][2];
+      v4 = curLevel["levelData"]["teleportPortal"][i][0][3];
+      ctx.fillRect(v1, v2, v3, v4);
+      if (RectCircleColliding({ x: playerX, y: playerY, r: playerSize }, { x: v1, y: v2, w: v3, h: v4 })) {
+        playerX = curLevel["levelData"]["teleportPortal"][0][2][0];
+        playerY = curLevel["levelData"]["teleportPortal"][0][2][1];
+      }
+      v1 = curLevel["levelData"]["teleportPortal"][i][1][0];
+      v2 = curLevel["levelData"]["teleportPortal"][i][1][1];
+      v3 = curLevel["levelData"]["teleportPortal"][i][1][2];
+      v4 = curLevel["levelData"]["teleportPortal"][i][1][3];
+      ctx.fillRect(v1, v2, v3, v4);
+      if (RectCircleColliding({ x: playerX, y: playerY, r: playerSize }, { x: v1, y: v2, w: v3, h: v4 })) {
+        playerX = curLevel["levelData"]["teleportPortal"][0][2][0];
+        playerY = curLevel["levelData"]["teleportPortal"][0][2][1];
+      }
+    }
+  }
+  for (let i = 0; i < curLevel["levelData"]["endFlag"].length; i++) {
+    if (hitFlags.includes(i)) {
+      ctx.fillStyle = "darkblue";
+    } else {
+      ctx.fillStyle = "blue";
+    }
+    flag1 = curLevel["levelData"]["endFlag"][i][0];
+    flag2 = curLevel["levelData"]["endFlag"][i][1];
+    flag3 = curLevel["levelData"]["endFlag"][i][2];
+    flag4 = curLevel["levelData"]["endFlag"][i][3];
+    ctx.fillRect(flag1, flag2, flag3, flag4);
+    if (RectCircleColliding({ x: playerX, y: playerY, r: playerSize }, { x: flag1, y: flag2, w: flag3, h: flag4 }) && !hitFlags.includes(i)) {
+      hitFlags.push(i);
+      if (hitFlags.length == curLevel["levelData"]["endFlag"].length) {
+        finish = true;
+      }
     }
   }
 }
@@ -164,6 +204,7 @@ function handleInput(canvas) {
   if (restart && curLevel) {
     playerX = curLevel["levelData"]["startPos"][0] ?? 0;
     playerY = curLevel["levelData"]["startPos"][1] ?? 0;
+    hitFlags = [];
     finish = false;
   }
   if (!finish) {
@@ -243,21 +284,21 @@ function loadMainPage(ctx) {
 
 function loadLevelsPage(ctx) {
   ctx.lineWidth = 5;
-  for(let i = 0; i < levels.length; i++) {
+  for (let i = 0; i < levels.length; i++) {
     ctx.fillStyle = "#101010";
     ctx.strokeStyle = "#7a7a7a";
-    ctx.rect(50 + (i%3)*195, 50 + Math.floor(i/3)*150, 150, 100);
+    ctx.rect(50 + (i % 3) * 195, 50 + Math.floor(i / 3) * 150, 150, 100);
     ctx.fill();
     ctx.stroke();
-    if (50 + (i%3)*195 < clickX && clickX < 200 + (i%3)*195 && 50 + Math.floor(i/3)*150 < clickY && clickY < 150 + Math.floor(i/3)*150) {
+    if (50 + (i % 3) * 195 < clickX && clickX < 200 + (i % 3) * 195 && 50 + Math.floor(i / 3) * 150 < clickY && clickY < 150 + Math.floor(i / 3) * 150) {
       state = -1;
       level = levelsShorthand[i];
-    };
+    }
   }
-  for(let i = 0; i < levels.length; i++) {
+  for (let i = 0; i < levels.length; i++) {
     ctx.font = "bold 40px Arial";
     ctx.fillStyle = "white";
-    ctx.fillText(levels[i], 60 + (i%3)*195, 115 + Math.floor(i/3)*150);
+    ctx.fillText(levels[i], 60 + (i % 3) * 195, 115 + Math.floor(i / 3) * 150);
   }
 }
 document.addEventListener("mousedown", (e) => {
